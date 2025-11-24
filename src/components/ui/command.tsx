641dad5f50d1c9efeee067d97270242a885da945
Tuhin Alom <tuhin.netmow@gmail.com>
2025-11-24 14:44:28 +0600

    type issues fixed

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Command as CommandPrimitive } from 'cmdk'
 import { SearchIcon } from 'lucide-react'
 import { cn } from '../../lib/utils'
-import type { Dialog , DialogContent,
+import { Dialog , DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle, } from './dialog'
