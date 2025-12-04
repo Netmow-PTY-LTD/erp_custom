994ee06cca98a8a206138c524a45e521de636f42
rabby <rabby.netmow@gmail.com>
2025-12-04 09:30:42 +0600

    user api service added

---
+++ src/types/users.ts
@@ -0,0 +1,15 @@
+
+export type Role = {
+  id: number;
+  name: string;
+  description: string;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role_id: number;
+  role: Role;
+  created_at: string; // or Date if you want: Date
+};
