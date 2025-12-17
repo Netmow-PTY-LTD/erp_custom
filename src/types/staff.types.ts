3be00ed0deca73eda59f6e02b90e650641d5633b
rabby <rabby.netmow@gmail.com>
2025-12-17 14:18:29 +0600

    staff details api integrate

--- src/types/staff.types.ts
+++ src/types/staff.types.ts
@@ -1,20 +1,8 @@
-// export type Staff = {
-//   hire_date: string | undefined;
-//   id: number;
-//   first_name: string;
-//   last_name: string;
-//   email: string;
-//   phone: string;
-//   position: string;
-//   department: string;
-//   salary: number;
-//   status: "active" | "inactive" | string; // You can expand if there are other statuses
-//   created_at: string; // ISO date string
-// };
 
 import type { Department } from "./types";
 
 export interface Staff {
+  address: string;
   id: number;
   employeeId: string;
   first_name: string;
