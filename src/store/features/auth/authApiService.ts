964aa0ba3f0d20a921f28da17b734312a5855c8f
Tuhin Alom <tuhin.netmow@gmail.com>
2026-01-09 18:44:23 +0600

    update profile api integrated

--- src/store/features/auth/authApiService.ts
+++ src/store/features/auth/authApiService.ts
@@ -1,5 +1,6 @@
 import type { AuthUserResponse, LoginRequest, LoginResponse } from "./types";
 import { baseApi } from "@/store/baseApi";
+import type { User } from "@/types/users.types";
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -16,7 +17,15 @@ export const authApi = baseApi.injectEndpoints({
         url: "/auth/me",
         method: "GET",
       }),
-      providesTags: ["Auth"],  
+      providesTags: ["Auth"],
+    }),
+    updateProfile: builder.mutation<AuthUserResponse, Partial<User>>({
+      query: (body) => ({
+        url: "/auth/profile",
+        method: "PUT", // Assuming PUT for updates
+        body,
+      }),
+      invalidatesTags: ["Auth"],
     }),
     authLogout: builder.mutation<void, void>({
       query: () => ({
@@ -28,4 +37,4 @@ export const authApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useAuthLoginMutation, useAuthLogoutMutation, useAuthUserQuery } = authApi;
+export const { useAuthLoginMutation, useAuthLogoutMutation, useAuthUserQuery, useUpdateProfileMutation } = authApi;
