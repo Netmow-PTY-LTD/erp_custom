99724a82b653e76a7b3d19b54c1de1b405e86196
Tuhin Alom <tuhin.netmow@gmail.com>
2026-01-30 21:32:08 +0600

    removed unused variable

--- src/pages/accounting/ChartOfAccounts.tsx
+++ src/pages/accounting/ChartOfAccounts.tsx
@@ -24,7 +24,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
 import CreateExpenseHeadForm from "./CreateExpenseHead";
